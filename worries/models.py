1c25cd9222c45e21bc8cdbb2774537fb401e0081
hw4nx02 <2002ckdghks@naver.com>
2026-06-16 17:36:17 +0900

    feat: 고민 모델 추가 및 고민 작성 메서드 추가

--- worries/models.py
+++ worries/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Worry(models.Model):
+    keyword = models.CharField(max_length=20)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    mbti = models.CharField(max_length=4)
+    pub_date = models.DateField(auto_now_add=True)
+    is_delete = models.BooleanField(default=False)
+    is_complete = models.BooleanField(default=False)
+    is_HoF = models.BooleanField(default=False)
+    cheerup = models.PositiveIntegerField(default=0)
+    gonggam = models.PositiveIntegerField(default=0)
+
+    # 20자 넘기면 ... 붙이고 요약
+    def summary(self):
+        str = self.content
+        if len(self.content) > 20:
+            str = str[:20] + "..."
+        return str
--- worries/views.py
+++ worries/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
 
-# Create your views here.
+def write_worry(request):
+    if not request.user.is_authenticated:
+        return redirect("accounts:login")
+    
+    new_worry = Worry()
+
+    new_worry.writer = request.user
+    new_worry.keyword = request.POST["keyword"]
+    new_worry.title = request.POST["title"]
+    new_worry.content = request.POST["content"]
+    new_worry.mbti = request.POST["mbti"]
+
+    new_worry.save()
+
+    return redirect("main:demo_firstpage.html")
