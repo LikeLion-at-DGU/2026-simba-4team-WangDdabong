ff4a382f3c5898c1369abd678c658ea90dd35cdd
hw4nx02 <2002ckdghks@naver.com>
2026-06-17 02:57:48 +0900

    refactor: 코드 주석 추가 및 리다이렉트 메서드 변경(firstpage->demo_home)

--- worries/models.py
+++ worries/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 
+"""
+고민
+"""
 class Worry(models.Model):
-    keyword = models.CharField(max_length=20)
-    title = models.CharField(max_length=50)
-    content = models.TextField()
-    mbti = models.CharField(max_length=4)
-    pub_date = models.DateField(auto_now_add=True)
-    is_delete = models.BooleanField(default=False)
-    is_complete = models.BooleanField(default=False)
-    is_HoF = models.BooleanField(default=False)
-    cheerup = models.PositiveIntegerField(default=0)
-    gonggam = models.PositiveIntegerField(default=0)
+    keyword = models.CharField(max_length=20)           # 키워드
+    title = models.CharField(max_length=50)             # 제목
+    content = models.TextField()                        # 본문
+    mbti = models.CharField(max_length=4)               # 선호 MBTI
+    pub_date = models.DateField(auto_now_add=True)      # 생성일
+    is_delete = models.BooleanField(default=False)      # 삭제 여부
+    is_complete = models.BooleanField(default=False)    # 완료 여부
+    is_HoF = models.BooleanField(default=False)         # 명예의 전당 등재 여부
+    cheerup = models.PositiveIntegerField(default=0)    # 응원 도장 개수
+    gonggam = models.PositiveIntegerField(default=0)    # 공감 도장 개수
 
     # 20자 넘기면 ... 붙이고 요약
     def summary(self):
--- worries/views.py
+++ worries/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 
+"""
+[고민 작성]
+- 기능: 작성한 내용으로 Worry 객체 생성 및 저장
+- 받는 값: keyword, title, content, mbti
+- return: 성공 -> demo_home 리다이렉트 / 실패(인증 에러) -> 로그인 메서드로 리다이렉트
+"""
 def write_worry(request):
     if not request.user.is_authenticated:
         return redirect("accounts:login")
@@ -15,4 +21,4 @@ def write_worry(request):
 
     new_worry.save()
 
-    return redirect("main:demo_firstpage")
+    return redirect("main:demo_home")
