e214d529357afeea6e472d50b72cafc913e85bee
Yunseo2727 <yunseo272727@naver.com>
2026-06-18 01:36:56 +0900

    feat: 고민 목록, 고민 상세, 고민 답변 페이지 UI 구현

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("worrywrite/", views.worrywrite, name="worrywrite"),
+    path("worry_write/", views.worry_write, name="worry_write"),
+    path("worry_list/", views.worry_list, name="worry_list"),
+    path("worry_detail/", views.worry_detail, name="worry_detail"),
+    path("worry_reply/", views.worry_reply, name="worry_reply"),
 ]
--- main/views.py
+++ main/views.py
@@ -3,8 +3,17 @@ from django.shortcuts import render, redirect
 def home(request):
     return render(request, "home.html")
 
-def worrywrite(request):
+def worry_write(request):
     if request.method == "POST":
         return redirect("home")
 
-    return render(request, "worrywrite.html")
+    return render(request, "worry_write.html")
+
+def worry_list(request):
+    return render(request, "worry_list.html")
+
+def worry_detail(request):
+    return render(request, "worry_detail.html")
+
+def worry_reply(request):
+    return render(request, "worry_reply.html")
