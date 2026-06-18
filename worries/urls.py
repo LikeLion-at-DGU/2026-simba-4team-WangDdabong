c1db2f6a5b0b3dd0b7d47de74d4748b5aba6e5de
hw4nx02 <2002ckdghks@naver.com>
2026-06-18 16:16:06 +0900

    fix: url에 인자 전달

--- worries/urls.py
+++ worries/urls.py
@@ -6,7 +6,7 @@ app_name = "worries"
 urlpatterns = [
     path("post_worry/", post_worry, name="post_worry"),
     path("get_worries/", get_worries, name="get_worries"),
-    path("post_bookmark/", post_bookmark, name="post_bookmark"),
-    path("post_cheerup/", post_cheerup, name="post_cheerup"),
-    path("get_worry_detail/", get_worry_detail, name="get_worry_detail"),
+    path("post_bookmark/<int:worry_id>", post_bookmark, name="post_bookmark"),
+    path("post_cheerup/<int:worry_id>", post_cheerup, name="post_cheerup"),
+    path("get_worry_detail/<int:worry_id>", get_worry_detail, name="get_worry_detail"),
 ]
