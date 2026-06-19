9f2b39b3cfa29216e4b4c5d920898edd086ae1e7
hw4nx02 <2002ckdghks@naver.com>
2026-06-19 22:32:51 +0900

    feat: 후일담 작성 기능 구현

--- writers/views.py
+++ writers/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
-from django.shortcuts import render , get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from accounts.models import Profile
+from worries.models import Worry
+from .models import *
 
 # Create your views here.
 
@@ -14,3 +16,22 @@ def mypage(request, id):
     }
 
     return render(request, 'writers/mypage.html', context)
+
+def post_epilogue(request, worry_id):
+    if not request.user.is_authenticated:
+        return redirect("accounts:login")
+    
+    worry = get_object_or_404(Worry, pk=worry_id)
+    worry.is_HoF = request.POST["is_HoF"]
+
+    new_epilogue = Epilogue()
+
+    new_epilogue.worry = worry
+    new_epilogue.writer = request.user
+    new_epilogue.ep_han_madi = request.POST["ep_han_madi"]
+    new_epilogue.ep_title = request.POST["ep_title"]
+    new_epilogue.ep_content = request.POST["ep_content"]
+
+    new_epilogue.save()
+
+    return redirect("main:demo_home", {"source": "post_epilogue"})
