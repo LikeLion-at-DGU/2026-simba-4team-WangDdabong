1d0047c84879235475c3642f64fae1f0a6079eac
14CES <2025111968@dgu.ac.kr>
2026-06-20 16:24:03 +0900

    fix : mypage url 수정

--- writers/urls.py
+++ writers/urls.py
@@ -4,7 +4,7 @@ from .views import *
 app_name = 'writers'
 
 urlpatterns = [
-    path('mypage/<int:id>', mypage, name='mypage'),
+    path('mypage/', mypage, name='mypage'),
     path('my_worry/', my_worry, name='my_worry'),
     path('my_answer/', my_answer, name='my_anser'),
     path('bookmark/', bookmark, name='bookmark'),
