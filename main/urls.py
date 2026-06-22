10f72e022d8ef230beebcea4a886ceb209908815
hw4nx02 <2002ckdghks@naver.com>
2026-06-22 23:15:06 +0900

    feat: 양 성장 과정 구매 관련 함수 url 등록

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,7 @@ app_name = 'main'
 
 urlpatterns = [
     path('', demo_home, name = 'demo_home'),
-    path("daily_attend/", daily_attend, name="daily_attend")
+    path("daily_attend/", daily_attend, name="daily_attend"),
+    path("get_yang_store/", get_yang_store, name="get_yang_store"),
+    path("post_buy_yang/<str:yang_id>", post_buy_yang, name="post_buy_yang"),
 ]
